486c40ba4b1ca3d4d4e8f00bb4277396d9208d3b
Thomas Masha <120525199+tmasha@users.noreply.github.com>
2026-02-11 14:37:20 -0600

    Combined Health, Ammo, and Quests UI scripts into one script, deleted the others

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+public class UIScript : MonoBehaviour
+{
+    [Header("Health UI")]
+    [SerializeField] private Slider healthBarSlider;
+    [SerializeField] private TextMeshProUGUI healthText;
+
+    [Header("Ammo UI")]
+    [SerializeField] private TextMeshProUGUI ammoText;
+
+    [Header("Quests UI")]
+    [SerializeField] private TextMeshProUGUI questText;
+
+    private PlayerController _playerController;
+    private Potato_Shooter _potatoShooter;
+
+    private readonly List<string> _quests = new List<string>();
+
+    void Start()
+    {
+        _playerController = FindFirstObjectByType<PlayerController>();
+        _potatoShooter = FindFirstObjectByType<Potato_Shooter>();
+
+        _quests.Add("End Waddle Quackdonald's entire career");
+        _quests.Add("Collect exotic meats from Marinara Trench");
+        _quests.Add("Find the star");
+    }
+
+    void Update()
+    {
+        if (_playerController != null)
+        {
+            if (healthBarSlider != null)
+            {
+                healthBarSlider.maxValue = _playerController.maxHealth;
+                healthBarSlider.value = _playerController.currentHealth;
+            }
+
+            if (healthText != null)
+                healthText.text = _playerController.currentHealth + " / " + _playerController.maxHealth;
+        }
+
+        if (_potatoShooter != null && ammoText != null)
+            ammoText.text = _potatoShooter.ammo + " / " + _potatoShooter.initialAmmo;
+
+        if (questText != null)
+            questText.text = "> " + string.Join("\n> ", _quests);
+    }
+}
