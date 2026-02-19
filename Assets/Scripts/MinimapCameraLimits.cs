b8f3dd3c002dbb9755f2ec38fcf6ce0c05e10cb8
Thomas Masha <120525199+tmasha@users.noreply.github.com>
2026-02-18 22:04:21 -0600

    Minimap now works when moving from MainScene to EnemyScene

--- Assets/Scripts/MinimapCameraLimits.cs
+++ Assets/Scripts/MinimapCameraLimits.cs
@@ -2,17 +2,34 @@ using UnityEngine;
 
 public class MinimapCameraLimits : MonoBehaviour
 {
-    
+    public static MinimapCameraLimits Instance { get; private set; }
+
     public GameObject player;
     public float yLimit;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Awake()
     {
-        
+        // Ensure only one minimap camera exists across scene loads
+        if (Instance != null && Instance != this)
+        {
+            Debug.LogWarning($"Duplicate MinimapCamera detected. Keeping '{Instance.gameObject.name}', destroying '{gameObject.name}'.");
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Update()
     {
         transform.position = new Vector3(player.transform.position.x, yLimit, player.transform.position.z);
     }
