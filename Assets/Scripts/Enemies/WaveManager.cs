a810697867f1e05eefc8f7ec068ac9cf892ce50e
Thomas Masha <120525199+tmasha@users.noreply.github.com>
2026-04-23 01:17:14 -0500

    show the current wave

--- Assets/Scripts/Enemies/WaveManager.cs
+++ Assets/Scripts/Enemies/WaveManager.cs
@@ -15,6 +15,14 @@ public class WaveManager : MonoBehaviour
 
     private IEnumerator WaveLoop()
     {
+        Win winScript = FindAnyObjectByType<Win>();
+        TMPro.TMP_Text winText = winScript?.GetComponent<TMPro.TMP_Text>();
+
+        if (winScript != null && GameManager.Instance != null)
+        {
+            winScript.UpdateWaveText(Mathf.Max(1, GameManager.Instance.CurrentWave() - 1), GameManager.Instance.LastWave());
+        }
+
         while (true)
         {
             foreach (var spawner in spawners)
@@ -25,13 +33,45 @@ public class WaveManager : MonoBehaviour
             // Increase difficulty
             GameManager.Instance.WaveCompleted();
 
+            if (winScript != null && GameManager.Instance != null)
+            {
+                winScript.UpdateWaveText(GameManager.Instance.CurrentWave() - 1, GameManager.Instance.LastWave());
+            }
+
             // Small delay before next wave
             if (GameManager.Instance.CurrentWave() == GameManager.Instance.LastWave())
             {
                 done = true;
                 yield break;
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
+
+            // Wait for all enemies to be defeated
+            while (FindObjectsByType<Enemy>(FindObjectsSortMode.None).Length > 0)
+            {
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            // Countdown to next wave
+            float countdown = timeBetweenWaves;
+            if (winText != null) 
+            {
+                winText.enabled = true;
+            }
+
+            while (countdown > 0)
+            {
+                if (winText != null)
+                {
+                    winText.text = $"Next wave in {Mathf.CeilToInt(countdown)} seconds";
+                }
+                yield return new WaitForSeconds(1f);
+                countdown -= 1f;
+            }
+
+            if (winText != null) 
+            {
+                winText.enabled = false;
+            }
         }
     }
 
